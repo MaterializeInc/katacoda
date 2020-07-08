ae60b40d7d30946c191500c6f01c6ace3aa3cb8c
Brandon W Maister <bwm@materialize.io>
2020-07-08 14:10:53 -0400

    Fix -w/--workers cli flag

--- intro-wikipedia/foreground.sh
+++ intro-wikipedia/foreground.sh
@@ -6,5 +6,5 @@ curl -L https://downloads.mtrlz.dev/materialized-v0.3.1-x86_64-unknown-linux-gnu
 # todo: move this to foreground for step2?
 #sudo apt-get update
 
-sudo apt-get install -y postgresql-client && echo "Installation complete! You can now go ahead and run Materialize: " && echo "   materialized --w=1  "
+sudo apt-get install -y postgresql-client && echo "Installation complete! You can now go ahead and run Materialize: " && echo "   materialized -w=1  "
 
