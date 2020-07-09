d164ff10e96953604937a1f32e1065f58c0c600c
Nikhil Benesch <nikhil.benesch@gmail.com>
2020-07-09 16:01:39 -0400

    Install postgresql-client-common too

--- intro-wikipedia/foreground.sh
+++ intro-wikipedia/foreground.sh
@@ -2,11 +2,18 @@
 
 set -euo pipefail
 
+pkgs=(
+    p/postgresql-common/postgresql-client-common_190ubuntu0.1_all.deb
+    p/postgresql-10/postgresql-client-10_10.12-0ubuntu0.18.04.1_amd64.deb
+)
+
 curl -fsSL https://downloads.mtrlz.dev/materialized-v0.3.1-x86_64-unknown-linux-gnu.tar.gz \
     | tar -xzC /usr/local --strip-components=1
 
-curl -fsSL http://security.ubuntu.com/ubuntu/pool/main/p/postgresql-10/postgresql-client-10_10.12-0ubuntu0.18.04.1_amd64.deb > postgresql-client.deb
-dpkg -i postgresql-client.deb
+for pkg in "${pkgs[@]}"; do
+    curl -fsSL http://security.ubuntu.com/ubuntu/pool/main/"$pkg" > pkg.deb
+    dpkg -i pkg.deb
+done
 
 echo "Installation complete! You can now go ahead and run Materialize:"
 echo
