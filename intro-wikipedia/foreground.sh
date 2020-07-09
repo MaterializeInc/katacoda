00b6ad4df3beb9e440bde3c43b054447331bd15e
Nikhil Benesch <nikhil.benesch@gmail.com>
2020-07-09 16:10:31 -0400

    Make setup less ugly

--- intro-wikipedia/foreground.sh
+++ intro-wikipedia/foreground.sh
@@ -1,22 +1,3 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
-pkgs=(
-    p/postgresql-common/postgresql-client-common_190ubuntu0.1_all.deb
-    p/postgresql-10/libpq5_10.12-0ubuntu0.18.04.1_amd64.deb
-    p/postgresql-10/postgresql-client-10_10.12-0ubuntu0.18.04.1_amd64.deb
-)
-
-curl -fsSL https://downloads.mtrlz.dev/materialized-v0.3.1-x86_64-unknown-linux-gnu.tar.gz \
-    | tar -xzC /usr/local --strip-components=1
-
-for pkg in "${pkgs[@]}"; do
-    curl -fsSL http://security.ubuntu.com/ubuntu/pool/main/"$pkg" > pkg.deb
-    dpkg -i pkg.deb
-done
-
-echo "Installation complete! You can now go ahead and run Materialize:"
-echo
-echo "    $ materialized -w 1"
-echo
+setup
