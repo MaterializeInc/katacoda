1ae0aaec6aa91c7bf3c39c8318c4238c2798e03e
Nikhil Benesch <nikhil.benesch@gmail.com>
2020-07-09 15:52:51 -0400

    Speed up installing PostgreSQL

--- intro-wikipedia/foreground.sh
+++ intro-wikipedia/foreground.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-curl -L https://downloads.mtrlz.dev/materialized-v0.3.1-x86_64-unknown-linux-gnu.tar.gz \
+set -euo pipefail
+
+curl -fsSL https://downloads.mtrlz.dev/materialized-v0.3.1-x86_64-unknown-linux-gnu.tar.gz \
     | tar -xzC /usr/local --strip-components=1
 
-# todo: move this to foreground for step2?
-#sudo apt-get update
+curl -fsSL http://security.ubuntu.com/ubuntu/pool/main/p/postgresql-10/postgresql-client-10_10.12-0ubuntu0.18.04.1_amd64.deb > postgresql-client.deb
+dpkg -i postgresql-client.deb
 
-sudo apt-get install -y postgresql-client && echo "Installation complete! You can now go ahead and run Materialize: " && echo "   materialized -w=1  "
-
+echo "Installation complete! You can now go ahead and run Materialize:"
+echo
+echo "    $ materialized -w 1"
+echo
