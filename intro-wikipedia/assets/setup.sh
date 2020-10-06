949dcab10d00f86988d2ab742b4d3fddbcab36c0
Nikhil Benesch <nikhil.benesch@gmail.com>
2020-10-06 17:35:15 -0400

    Installation order is important

--- intro-wikipedia/assets/setup.sh
+++ intro-wikipedia/assets/setup.sh
@@ -2,13 +2,19 @@
 
 set -euo pipefail
 
+pkgs=(
+    postgresql-client-common_190ubuntu0.1_all.deb
+    libpq5_10.14-0ubuntu0.18.04.1_amd64.deb
+    postgresql-client-10_10.14-0ubuntu0.18.04.1_amd64.deb
+)
+
 # Prevent package installation from updating the man-db, which is slow.
 echo "Configuring system..."
 apt-get remove --purge man-db -qy > /dev/null
 
 echo "Installing PostgreSQL..."
-for pkg in /usr/local/dpkg/*; do
-    dpkg -i "$pkg"
+for pkg in "${pkgs[@]}"; do
+    dpkg -i "/usr/local/dpkg/$pkg"
 done
 
 echo "Installing Materialize..."
