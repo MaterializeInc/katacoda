a9602fdadeb3024c8381b7f05222418ced35e51e
Nikhil Benesch <nikhil.benesch@gmail.com>
2020-10-06 17:19:40 -0400

    Harden installation
    Check in the PostgreSQL deb packages to be resistant against future
    updates to Ubuntu 18.04 LTS. (PostgreSQL does receive security updates
    periodically.)
    
    Also dynamically fetch the latest Materialize version, and download that
    version, so that we don't need to remember to update this repo when we
    release a new version.

--- intro-wikipedia/assets/setup.sh
+++ intro-wikipedia/assets/setup.sh
@@ -2,23 +2,18 @@
 
 set -euo pipefail
 
-pkgs=(
-    p/postgresql-common/postgresql-client-common_190ubuntu0.1_all.deb
-    p/postgresql-10/libpq5_10.14-0ubuntu0.18.04.1_amd64.deb
-    p/postgresql-10/postgresql-client-10_10.14-0ubuntu0.18.04.1_amd64.deb
-)
-
 # Prevent package installation from updating the man-db, which is slow.
 apt-get remove --purge man-db -qy > /dev/null
 
 echo "Installing PostgreSQL..."
-for pkg in "${pkgs[@]}"; do
-    curl -fsSL http://security.ubuntu.com/ubuntu/pool/main/"$pkg" > pkg.deb
-    dpkg -i pkg.deb
+for pkg in assets/*; do
+    dpkg -i "$pkg"
 done
 
 echo "Installing Materialize..."
-curl -fsSL https://downloads.mtrlz.dev/materialized-v0.4.3-x86_64-unknown-linux-gnu.tar.gz \
+version=$(curl -fsSL https://materialize.io/docs/versions.json | jq -r .[0].name)
+echo "Latest stable release: $version"
+curl -fsSL https://downloads.mtrlz.dev/materialized-"$version"-x86_64-unknown-linux-gnu.tar.gz \
     | tar -xzC /usr/local --strip-components=1
 
 echo "Installation complete! You can now go ahead and run Materialize:"
