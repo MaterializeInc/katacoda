9d8d1955eed57e6edcf45d3defc7305a43f32451
Nikhil Benesch <nikhil.benesch@gmail.com>
2020-10-06 15:18:15 -0400

    Fix installation

--- intro-wikipedia/assets/setup.sh
+++ intro-wikipedia/assets/setup.sh
@@ -4,9 +4,12 @@ set -euo pipefail
 
 pkgs=(
     p/postgresql-common/postgresql-client-common_190ubuntu0.1_all.deb
-    p/postgresql-10/libpq5_10.12-0ubuntu0.18.04.1_amd64.deb
-    p/postgresql-10/postgresql-client-10_10.12-0ubuntu0.18.04.1_amd64.deb
+    p/postgresql-10/libpq5_10.14-0ubuntu0.18.04.1_amd64.deb
+    p/postgresql-10/postgresql-client-10_10.14-0ubuntu0.18.04.1_amd64.deb
 )
+
+# Prevent package installation from updating the man-db, which is slow.
+apt-get remove --purge man-db
 
 echo "Installing PostgreSQL..."
 for pkg in "${pkgs[@]}"; do
